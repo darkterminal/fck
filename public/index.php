ece41c8bd90702e123b0614b61400dcd388d6671
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-12-30 01:03:46 +0700

    feat: also add new fck auth routes

--- public/index.php
+++ public/index.php
@@ -10,4 +10,10 @@ $app->router->get('/', 'SiteController@home');
 $app->router->get('/contact', 'SiteController@contact');
 $app->router->post('/contact', 'SiteController@handleContact');
 
+$app->router->get('/login', 'AuthController@login');
+$app->router->post('/login', 'AuthController@login');
+
+$app->router->get('/register', 'AuthController@register');
+$app->router->post('/register', 'AuthController@register');
+
 $app->run();
