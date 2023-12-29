87814c446de7642c0364c5c18bd0278ddedbea3f
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-12-30 04:43:29 +0700

    feat: create a fck database connection

--- core/Application.php
+++ core/Application.php
@@ -10,15 +10,17 @@ class Application
     public Router $router;
     public Request $request;
     public Response $response;
+    public Database $db;
     public Controller $controller;
 
-    public function __construct($rootPath)
+    public function __construct($rootPath, array $config)
     {
         self::$app = $this;
         self::$ROOT_DIR = $rootPath;
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
+        $this->db = new Database($config['db']);
     }
 
     public function getController(): Controller {
