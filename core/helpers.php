8c9853473239612580f07b3c11a42fd335319216
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-12-30 01:01:50 +0700

    feat: add new fck helpers

--- core/helpers.php
+++ core/helpers.php
@@ -7,3 +7,17 @@ function dump($data)
     echo "</pre>";
     die();
 }
+
+function text_alt_formatter($input)
+{
+    // Convert camelCase or PascalCase to snake_case
+    $snakeCase = strtolower(preg_replace('/([a-z])([A-Z])/', '$1_$2', $input));
+
+    // Explode snake_case into an array
+    $exploded = explode('_', $snakeCase);
+
+    // Capitalize the first letter of each word
+    $capitalizedWords = array_map('ucfirst', $exploded);
+
+    return implode(' ', $capitalizedWords);
+}
