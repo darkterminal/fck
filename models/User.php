87c6477ce21ca7d183d6008e7a229c0ae451a33e
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-12-31 13:44:41 +0700

    feat: add the fck user model

---
+++ models/User.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Fckin\models;
+
+use Fckin\core\Model;
+
+class User extends Model
+{
+    public string $firstName = '';
+    public string $lastName = '';
+    public string $email = '';
+    public string $password = '';
+    public string $repeatPassword = '';
+
+    public string $tableName = 'users';
+
+    public function rules(): array
+    {
+        return [
+            'firstName' => [self::RULE_REQUIRED],
+            'lastName' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL, [self::RULE_UNIQUE, 'class' => self::class]],
+            'password' => [self::RULE_REQUIRED, [self::RULE_MIN, 'min' => 8], [self::RULE_MAX, 'max' => 24]],
+            'repeatPassword' => [self::RULE_REQUIRED, [self::RULE_MATCH, 'match' => 'password']],
+        ];
+    }
+
+    public function register() {
+        $result = $this->table($this->tableName)->insert([
+            'firstName' => $this->firstName,
+            'lastName' => $this->lastName,
+            'email' => $this->email,
+            'password' => password_hash($this->password, PASSWORD_DEFAULT)
+        ]);
+        return $result > 0;
+    }
+}
