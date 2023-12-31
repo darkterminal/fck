49992520905fad8244aa849f3e9369cdbc385540
Imam Ali Mustofa <d.darkterminal@gmail.com>
2023-12-31 13:45:01 +0700

    feat: add new fck login page

---
+++ models/Login.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Fckin\models;
+
+use Fckin\core\FTAuth;
+use Fckin\core\Model;
+
+class Login extends Model
+{
+    public string $email = '';
+    public string $password = '';
+
+    public string $tableName = 'users';
+
+    public function rules(): array
+    {
+        return [
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'password' => [self::RULE_REQUIRED],
+        ];
+    }
+
+    public function login()
+    {
+        $user = $this->table($this->tableName)->select('*')->where('email', '=', $this->email)->get();
+        if (!$user) {
+            $this->addError('email', 'User does not exist with this email');
+            return false;
+        }
+
+        if (!password_verify($this->password, $user->password)) {
+            $this->addError('password', 'Password is incorrect!');
+            return false;
+        }
+
+        $auth = new FTAuth('randomsecretovertheworld0123456789');
+        $token = $auth->generateToken($user->id);
+        return $auth->setAuth($token);
+    }
+}
