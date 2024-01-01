ecf7594a2b8dd94a8719f1e320a25abc3d72f916
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-01 17:12:34 +0700

    refactor(Controller): update render method to use View from the fck Application

--- core/Controller.php
+++ core/Controller.php
@@ -12,7 +12,7 @@ class Controller {
     public string $action = '';
 
     public function render($view, $params = []) {
-        return Application::$app->router->renderView($view, $params);
+        return Application::$app->view->renderView($view, $params);
     }
 
     public function setLayout($layout) {
