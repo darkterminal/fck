e58e6ef60f6a0765726160e481cfb6b2a3be479e
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-01 17:25:10 +0700

    feat(models): add the fck Contact model

---
+++ models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fckin\models;
+
+use Fckin\core\db\Model;
+
+class Contact extends Model
+{
+    public string $subject = '';
+    public string $email = '';
+    public string $message = '';
+
+    public string $tableName = 'contacts';
+
+    public function rules(): array
+    {
+        return [
+            'subject' => [self::RULE_REQUIRED],
+            'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
+            'message' => [self::RULE_REQUIRED],
+        ];
+    }
+}
