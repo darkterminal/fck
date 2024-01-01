3e7ba61eee9474c292b356f75949877b085650d6
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-01 17:24:32 +0700

    feat(routes): add the fck web.php

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+$app->router->get('/', 'SiteController@home');
+$app->router->get('/contact', 'SiteController@contact');
+
+$app->router->get('/login', 'AuthController@login');
+$app->router->post('/login', 'AuthController@login');
+
+$app->router->get('/register', 'AuthController@register');
+$app->router->post('/register', 'AuthController@register');
+
+$app->router->get('/profile', 'AuthController@profile');
+
+$app->router->get('/logout', 'AuthController@logout');
