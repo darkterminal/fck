24115f83a189684cad8aae5f633c6108e5f5b78d
Imam Ali Mustofa <d.darkterminal@gmail.com>
2024-01-07 20:20:15 +0700

    feat(model): add the fck. job attribute to Contact model

--- models/Contact.php
+++ models/Contact.php
@@ -9,6 +9,7 @@ class Contact extends Model
     public string $subject = '';
     public string $email = '';
     public string $message = '';
+    public string $job = '';
 
     public string $tableName = 'contacts';
 
@@ -18,6 +19,7 @@ class Contact extends Model
             'subject' => [self::RULE_REQUIRED],
             'email' => [self::RULE_REQUIRED, self::RULE_EMAIL],
             'message' => [self::RULE_REQUIRED],
+            'job' => [self::RULE_REQUIRED],
         ];
     }
 }
